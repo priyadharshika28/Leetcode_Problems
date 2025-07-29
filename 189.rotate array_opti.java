010a826c896f1d488169bc24404592ffdf4185f4
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-07-29 21:40:27 +0530

    Create 189.rotate array_opti.java

---
+++ 189.rotate array_opti.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k%n;
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+    public void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
