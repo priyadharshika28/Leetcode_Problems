c18ccadcfe27caf0271ee43435ad3b9f92fb3248
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-07-30 22:00:04 +0530

    Create 7.reverse integer0ms.java

---
+++ 7.reverse integer0ms.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int n) {
+        int rem = 0;
+        int rev=0;
+        while(n!=0){
+            rem = n%10;
+            if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            rev=rev*10+rem;
+            n/=10;
+        }
+        return rev;
+    }
+}
